e4c8dc4d81bcf6ac9e79387bf1f67e918363a112
Roman <roman@burningswell.com>
2012-03-19 18:40:22 +0100

    Switched to Clojure 1.3, Ring 1.0.2 and cheshire 3.0.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
-(defproject ring-middleware-format "0.2.0-SNAPSHOT"
-  :description "Ring middleware for parsing parameters and emitting
-  responses in various formats. See
-  https://github.com/ngrunwald/ring-middleware-format"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+(defproject ring-middleware-format "0.2.0"
+  :description "Ring middleware for parsing parameters and emitting responses in various formats. See https://github.com/ngrunwald/ring-middleware-format"
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.memoize "0.5.1"]
-                 [ring/ring-core "1.0.2"]
-                 [cheshire "4.0.0"]
-                 [clj-yaml "0.3.1"]])
+                 [ring/ring-core "1.1.0"]
+                 [cheshire "4.0.0"]])
